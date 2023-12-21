5499164a37267cee8c267d0264075950271f0eb2
tinchourteaga <38136900+tinchourteaga@users.noreply.github.com>
2023-12-21 17:47:04 -0300

    initial project structure + proto files, config, db

---
+++ pkg/db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Connector struct {
+	DB *gorm.DB
+}
+
+func NewDatabaseConnection() Connector {
+	db, err := gorm.Open(postgres.Open(viper.GetString("DB_URL")), &gorm.Config{})
+	if err != nil {
+		log.Fatal().Msg("cannot connect to database")
+	}
+
+	//db.AutoMigrate(&models.Product{}, &models.StockDecreaseLog{})
+
+	return Connector{DB: db}
+}
